b8745bac2c30267dee01505fb6eb4066852659df
Akash Mair <akash.mair@and.digital>
2021-01-12 08:20:00 +0000

    styling

--- src/Client/Index.fs
+++ src/Client/Index.fs
@@ -159,14 +159,14 @@ let view (model: Model) (dispatch: Msg -> unit) =
                     Hero.Props [ Style [ Background
                                              """linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d20d9193-3ffa-4a3a-915d-5312204873a7/d57lvld-b530ad43-7a5f-4be9-bd2b-0ffd9a83983d.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvZDIwZDkxOTMtM2ZmYS00YTNhLTkxNWQtNTMxMjIwNDg3M2E3XC9kNTdsdmxkLWI1MzBhZDQzLTdhNWYtNGJlOS1iZDJiLTBmZmQ5YTgzOTgzZC5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.i5murD_hh7VxoNNDgB4W6SEGteWLAAv058ypRGBvVVU")""" ] ] ] [
             Hero.body [] [
-                Columns.columns [ Columns.IsCentered
-                                  Columns.IsVCentered ] [
-                    Column.column [ Column.Width(Screen.All, Column.Is1) ] [
-                        Button.button [ Button.Size IsLarge
-                                        Button.Color IsPrimary
-                                        Button.OnClick(fun _ -> StartClicked |> dispatch) ] [
-                            str "Start"
-                        ]
+                Container.container [ Container.Modifiers [ Modifier.TextAlignment(Screen.All, TextAlignment.Centered) ]
+                                      Container.IsWideScreen ] [
+
+                    Button.button [ Button.Modifiers [ Modifier.TextAlignment(Screen.All, TextAlignment.Centered) ]
+                                    Button.Size IsLarge
+                                    Button.Color IsPrimary
+                                    Button.OnClick(fun _ -> StartClicked |> dispatch) ] [
+                        str "Start"
                     ]
                 ]
             ]
@@ -200,18 +200,18 @@ let view (model: Model) (dispatch: Msg -> unit) =
         Hero.hero [ Hero.IsFullHeight
                     Hero.Props [ Style [ Background
                                              """linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("https://ninfora.com/forums/uploads/monthly_2016_09/2d6eba2c-e254-4271-b7ca-25f5a96ef4e0.png.5c7faa57f79402eccf50c4cc2e47b0e0.png")""" ] ] ] [
+
             Hero.body [] [
-                Columns.columns [ Columns.IsCentered
-                                  Columns.IsVCentered ] [
-                    Column.column [] [
-                        Heading.h1 [ Heading.Modifiers [ Modifier.TextColor IsBlack ] ] [
-                            str (sprintf "Score: %i" score)
-                        ]
-                        Button.button [ Button.Color IsDanger
-                                        Button.Size IsLarge
-                                        Button.OnClick(fun _ -> StartClicked |> dispatch) ] [
-                            str "Restart"
-                        ]
+                Container.container [ Container.Modifiers [ Modifier.TextAlignment(Screen.All, TextAlignment.Centered) ]
+                                      Container.IsWideScreen ] [
+                    Heading.h1 [ Heading.Modifiers [ Modifier.TextColor IsDanger ] ] [
+                        str (sprintf "Score: %i" score)
+                    ]
+                    Button.button [ Button.Modifiers [ Modifier.TextAlignment(Screen.All, TextAlignment.Centered) ]
+                                    Button.Size IsLarge
+                                    Button.Color IsDanger
+                                    Button.OnClick(fun _ -> StartClicked |> dispatch) ] [
+                        str "Restart"
                     ]
                 ]
             ]
